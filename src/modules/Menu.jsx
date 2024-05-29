97b27a369c15aebdb385a87d815e8235d105ff3a
xSStephen <pufuletidinamici@gmail.com>
2024-05-29 23:43:07 +0300

    update

--- src/modules/Menu.jsx
+++ src/modules/Menu.jsx
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div>
+      <div className="settings">
+        <XMarkIcon width={30} height={30} />
+        <p>Settings</p>
+      </div>
+      <div className="app">
+        <h3>APP</h3>
+        <ul>
+          <li>General</li>
+          <li>Theme</li>
+          <li>Reorder habits</li>
+        </ul>
+      </div>
+      <div className="general">
+        <h3>General</h3>
+        <ul>
+          <li>Website</li>
+          <li>Terms of use</li>
+          <li>Rate the app</li>
+          <li>Credits</li>
+          <li>Send feedback</li>
+          <li>Contribute to the project</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
